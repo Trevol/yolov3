0746e44781c0eca4f69f82757cb47028da9d0a98
trevol <iamvovan@mail.ru>
2020-08-04 23:21:09 +0300

    app prototype

--- utils/datasets.py
+++ utils/datasets.py
@@ -628,21 +628,23 @@ def letterbox(img, new_shape=(416, 416), color=(114, 114, 114), auto=True, scale
     if isinstance(new_shape, int):
         new_shape = (new_shape, new_shape)
 
-    # Scale ratio (new / old)
+    # Scale wh_ratio (new / old)
     r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
     if not scaleup:  # only scale down, do not scale up (for better test mAP)
         r = min(r, 1.0)
 
     # Compute padding
-    ratio = r, r  # width, height ratios
+    wh_ratio = r, r  # width, height ratios
     new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
     dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
     if auto:  # minimum rectangle
         dw, dh = np.mod(dw, 64), np.mod(dh, 64)  # wh padding
     elif scaleFill:  # stretch
         dw, dh = 0.0, 0.0
-        new_unpad = new_shape
-        ratio = new_shape[0] / shape[1], new_shape[1] / shape[0]  # width, height ratios
+        # new_unpad = new_shape
+        # wh_ratio = new_shape[0] / shape[1], new_shape[1] / shape[0]  # width, height ratios
+        new_unpad = new_shape[::-1]
+        wh_ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]
 
     dw /= 2  # divide padding into 2 sides
     dh /= 2
@@ -652,7 +654,7 @@ def letterbox(img, new_shape=(416, 416), color=(114, 114, 114), auto=True, scale
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
-    return img, ratio, (dw, dh)
+    return img, wh_ratio, (dw, dh)
 
 
 def random_affine(img, targets=(), degrees=10, translate=.1, scale=.1, shear=10, border=0):
